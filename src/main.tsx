9a182f97c7fba233525e531cb7282dbf59d5a3b3
sanli-11 <sanli.has11@gmail.com>
2025-02-09 10:03:32 +0500

    Setup TailwindCSS

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const root = document.getElementById("#root") as HTMLElement;
+const root = document.getElementById("root") as HTMLElement;
 
 createRoot(root).render(
   <StrictMode>
