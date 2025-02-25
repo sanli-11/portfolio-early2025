f66ddc634911d238d9252d7da43cf52a74198b93
sanli-11 <sanli.has11@gmail.com>
2025-02-25 06:55:38 +0500

    Created and appended About. Set background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import About from "./components/About";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="relative py-6">
       <Header />
-      <main className="px-4 py-8"></main>
+      <main className="px-4 py-8">
+        <About />
+      </main>
     </div>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <article className="absolute top-0 left-0 h-dvh w-full bg-[url(./assets/images/grid.svg)]">
+      <div className="to-palette-6 via-palette-6/75 absolute top-0 left-0 size-full bg-radial-[at_50%_35%] from-transparent from-20%" />
+      <section></section>
+    </article>
+  );
+}
+
+export default About;
